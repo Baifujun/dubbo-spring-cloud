dbeab135c5b9e6ed9524266727a484c69c0534b2
baifj <mayucheng@jiudianbang.com>
2021-02-02 18:26:01 +0800

    dubbo与SpringCloud整合

---
+++ producer/src/test/java/com/baixs/producer/ProducerTests.java
@@ -0,0 +1,11 @@
+package com.baixs.producer;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProducerTests {
+
+    //@Test
+    public void test() {
+    }
+}
